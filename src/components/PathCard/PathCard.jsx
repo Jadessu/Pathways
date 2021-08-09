8c260d3608f87ffba245bf9e41a7fc739b786e87
jadessu <junioradessu@gmail.com>
2021-08-09 14:42:15 -0500

    card design

--- src/components/PathCard/PathCard.jsx
+++ src/components/PathCard/PathCard.jsx
@@ -15,7 +15,31 @@ const PathCard = (props) => {
 
   return (
     <>
-    
+    <div className="path-container">
+      <div className="path-card">
+        <div className="path-image">
+          <img src={image}alt="" />
+        </div>
+        <div className="path-body">
+          <div className="path-info">
+
+          <h1>WEB DEVELOPMENT</h1>
+                <p>blah blah blah some info on the pathway Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci animi assumenda cumque deserunt
+                    dolorum ex exercitationem.</p>
+          </div>
+          <div className="skill-infos">
+            <div className="skills">
+              <button className="card-button">VIEW SKILLS
+              <span>&rarr;</span>
+              </button>
+             
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
 
     </>
   )
